8315e0b74e847364128a5278797e1626975134c9
aherbert <a.herbert@sussex.ac.uk>
2018-08-30 13:04:54 +0100

    Delegate URI encoding to java.net

--- src/main/java/uk/ac/sussex/gdsc/analytics/URIEncoder.java
+++ src/main/java/uk/ac/sussex/gdsc/analytics/URIEncoder.java
@@ -28,34 +28,139 @@
  */
 package uk.ac.sussex.gdsc.analytics;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.BitSet;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 /**
- * Simple uri encoder, made from the spec at:
- * http://www.ietf.org/rfc/rfc2396.txt
- *
- * @author Daniel Murphy
+ * Simple URI encoder to encode strings using UTF-8.
  */
 public class URIEncoder {
-    private static String mark = "-_.!~*'()\"";
+
+    /** The characters that do not require encoding. */
+    private final static BitSet dontNeedEncoding;
+
+    static {
+
+        // Copied from java.net.URLEncoder:
+
+        /*
+         * The list of characters that are not encoded has been determined as follows:
+         *
+         * RFC 2396 states: ----- Data characters that are allowed in a URI but do not
+         * have a reserved purpose are called unreserved. These include upper and lower
+         * case letters, decimal digits, and a limited set of punctuation marks and
+         * symbols.
+         *
+         * unreserved = alphanum | mark
+         *
+         * mark = "-" | "_" | "." | "!" | "~" | "*" | "'" | "(" | ")"
+         *
+         * Unreserved characters can be escaped without changing the semantics of the
+         * URI, but this should not be done unless the URI is being used in a context
+         * that does not allow the unescaped character to appear. -----
+         *
+         * It appears that both Netscape and Internet Explorer escape all special
+         * characters from this list with the exception of "-", "_", ".", "*". While it
+         * is not clear why they are escaping the other characters, perhaps it is safest
+         * to assume that there might be contexts in which the others are unsafe if not
+         * escaped. Therefore, we will use the same list. It is also noteworthy that
+         * this is consistent with O'Reilly's "HTML: The Definitive Guide" (page 164).
+         *
+         * As a last note, Intenet Explorer does not encode the "@" character which is
+         * clearly not unreserved according to the RFC. We are being consistent with the
+         * RFC in this matter, as is Netscape.
+         *
+         */
+
+        dontNeedEncoding = new BitSet(256);
+        int i;
+        for (i = 'a'; i <= 'z'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        for (i = 'A'; i <= 'Z'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        for (i = '0'; i <= '9'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        // Delegate the required encoding of space to URLEncoder
+        // dontNeedEncoding.set(' ');
+        dontNeedEncoding.set('-');
+        dontNeedEncoding.set('_');
+        dontNeedEncoding.set('.');
+        dontNeedEncoding.set('*');
+    }
 
     /**
-     * Encode the string
+     * No public instances
+     */
+    private URIEncoder() {
+        // Do nothing
+    }
+
+    /**
+     * Encode the string using UTF-8.
+     * 
+     * <p>A check is made for any characters that require encoding. If {@code false}
+     * the same string is returned.
+     * 
+     * <p>Otherwise the actual encoding is performed by
+     * {@link URLEncoder#encode(String, String)}.
      *
      * @param string The string
      * @return The encoded string
      */
     public static String encodeURI(String string) {
-        final StringBuilder uri = new StringBuilder(); // Encoded URL
-
-        final char[] chars = string.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            final char c = chars[i];
-            if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || mark.indexOf(c) != -1)
-                uri.append(c);
-            else {
-                uri.append('%');
-                uri.append(Integer.toHexString(c));
-            }
+        if (noEncodingRequired(string)) {
+            return string;
         }
-        return uri.toString();
+        return encodeURI(string, "UTF-8");
+    }
+
+    /**
+     * Encode the string using the given encoding.
+     *
+     * @param string   The string
+     * @param encoding The encoding (UTF-8 is recommended)
+     * @return The encoded string
+     * @throws RuntimeException If the encoding is not supported
+     */
+    static String encodeURI(String string, String encoding) throws RuntimeException {
+        try {
+            return URLEncoder.encode(string, encoding);
+        } catch (UnsupportedEncodingException e) {
+            // UTF-8 is required by the Java platform so this should not happen
+            Logger.getLogger(URIEncoder.class.getName()).severe("Unsupported encoding: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Check if the string contains only characters that will not be encoded.
+     *
+     * @param s the string
+     * @return true, if no encoded characters are present
+     */
+    public static boolean noEncodingRequired(String s) {
+        Objects.requireNonNull(s, "The string is null");
+        for (int i = 0; i < s.length(); i++) {
+            if (!noEncodingRequired(s.charAt(i)))
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Check if the character will not be encoded.
+     *
+     * @param c the character
+     * @return true, if no encoding is required
+     */
+    @SuppressWarnings("cast")
+    public static boolean noEncodingRequired(char c) {
+        return dontNeedEncoding.get((int) c);
     }
 }
--- src/test/java/uk/ac/sussex/gdsc/analytics/URIEncoderTest.java
+++ src/test/java/uk/ac/sussex/gdsc/analytics/URIEncoderTest.java
@@ -28,13 +28,21 @@
  */
 package uk.ac.sussex.gdsc.analytics;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("javadoc")
 public class URIEncoderTest {
+
+    private Logger logger = Logger.getLogger(URIEncoderTest.class.getName());
+    private Level level = Level.FINE;
+
     @Test
     public void testEncoder() throws Exception {
         // Create a string that should not be encoded
@@ -45,28 +53,70 @@ public class URIEncoderTest {
             sb.append(c);
         for (char c = 'A'; c <= 'Z'; c++)
             sb.append(c);
-        sb.append("-_.!~*'()\"");
+        sb.append("-_.*");
         final String unencoded = sb.toString();
         Assertions.assertTrue(unencoded.length() > 0);
-        Assertions.assertEquals(unencoded, URIEncoder.encodeURI(unencoded));
+        Assertions.assertTrue(URIEncoder.noEncodingRequired(unencoded));
+        // The same string
+        Assertions.assertSame(unencoded, URIEncoder.encodeURI(unencoded));
 
-        // Encode others as hex
+        // Encode others as hex in UTF-8
         testEncode(" ");
         testEncode("&");
         testEncode("@");
         testEncode("/");
         testEncode("[]{}%$#");
+        testEncode("normal []{}%$# text");
+        testEncode("normal [a]b{c}d%e$f# text");
+        testEncode("complex ἀ Ģ ↛");
+
+        // Try and break it with extended characters in UTF-8
+        testEncode((char) 0x0000);
+        testEncode((char) 0x0001);
+        testEncode((char) 0x0010);
+        testEncode((char) 0x0100);
+        testEncode((char) 0x1000);
+        testEncode((char) 0x000F);
+        testEncode((char) 0x00F0);
+        testEncode((char) 0x0F00);
+        testEncode((char) 0xF000);
+        testEncode((char) 0xFFFF);
+        testEncode((char) 0xFFF0);
+        testEncode((char) 0xFF00);
+        testEncode((char) 0xF000);
     }
 
-    private static void testEncode(String string) throws Exception {
+    private void testEncode(String string) throws Exception {
         // The encoding is not standard. It encodes all unsupported
         // chars as hex. So encode and test it can be decoded to the
         // same string using a standard decoder.
         final String encoded = URIEncoder.encodeURI(string);
+
+        // Log the encoding
+        logger.log(level, () -> {
+            String answer;
+            try {
+                answer = URLEncoder.encode(string, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+            return "<<" + string + ">> == <<" + answer + ">> : <<" + encoded + ">>";
+        });
         Assertions.assertEquals(string, decode(encoded));
+    }
+
+    private void testEncode(char c) throws Exception {
+        testEncode(new String(new char[] { c }));
     }
 
     private static String decode(String s) throws Exception {
         return URLDecoder.decode(s, "UTF-8");
+    }
+
+    @Test
+    public void testEncoderThrows() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            URIEncoder.encodeURI("test", "bad charset name");
+        });
     }
 }
